81edd2df6225cb5adc6ebbc72e19538c089e2bee
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-02-19 00:53:29 +0100

    init

---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link text-9xl">Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
