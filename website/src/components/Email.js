4f843f336878b032725dd04d77ec1ff93f56b751
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-03-05 01:56:59 +0100

    Working sending email!

--- website/src/components/Email.js
+++ website/src/components/Email.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Email = () => {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current, {
+                publicKey: process.env.REACT_APP_EMAILJS_USER_ID,
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+        e.target.reset()
+    };
+
     return (
         <section className="h-screen flex justify-center items-center text-custom-dark dark:text-white font-body ">
             <div className='container max-w-[1240px]'>
                 <h2 className='text-center mb-6 md:text-5xl sm:text-4xl text-3xl font-bold py-2'>Contact us</h2>
-                <form className='flex flex-col gap-4 justify-between w-full p-3 shadow-sm sm:text-xl md:text-2xl'>
+                <form
+                    ref={form}
+                    onSubmit={sendEmail}
+                    className='flex flex-col gap-4 justify-between w-full p-3 shadow-sm sm:text-xl md:text-2xl'>
 
                     <div>
                         <label for="subject" class="block font-medium text-gray-900 dark:text-gray-300">Name</label>
