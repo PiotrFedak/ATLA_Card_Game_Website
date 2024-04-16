176a413671e712af3ec294419c3e21ce6e7e457f
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-04-16 22:15:16 +0200

    Some api changes

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api'
+    baseURL: 'https://apiatlaphp.000webhostapp.com/api'
 });
 
 axiosClient.interceptors.request.use((config) => {
