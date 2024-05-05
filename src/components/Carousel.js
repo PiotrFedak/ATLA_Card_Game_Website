bbb460d47489ef12ff5ae9e18567fcbcac5ef475
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-05-05 14:14:37 +0200

    Iterator pattern

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,39 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { characters } from './charactersData';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import CharacterIterator from './CharacterIterator';
 
 const Carousel = () => {
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
+    const characterIterator = useRef(new CharacterIterator(characters));
 
     const goToNextCards = () => {
-        setCurrentCardIndex((prevIndex) => (prevIndex + 3) % characters.length);
+        characterIterator.current.next();
+        setCurrentCardIndex(characterIterator.current.currentIndex);
     };
 
     const goToPreviousCards = () => {
-        setCurrentCardIndex((prevIndex) => (prevIndex - 3 + characters.length) % characters.length);
+        characterIterator.current.previous();
+        setCurrentCardIndex(characterIterator.current.currentIndex);
     };
 
     return (
-        <div id="carousel" className='w-full py-[10rem] px-4 bg-white dark:bg-custom-black text-custom-dark dark:text-white relative'>
-            <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8 relative'>
-                {characters.slice(currentCardIndex, currentCardIndex + 3).map((character, index) => (
-                    <div key={index} className={`w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 border border-oraange`}>
-                        <img className='w-220 mx-auto mt-[-3rem]' src={character.image} alt={character.name} />
-                        <h2 className={`text-2xl font-bold text-center text-${character.color}-500 py-8`}>{character.name}</h2>
-                        <p className='text-center text-4xl font-bold '>{character.type}</p>
-                        <div className='text-center font-medium'>
-                            {character.abilities.map((ability, index) => (
-                                <p key={index} className='py-2 pt-8 border-b mx-8'>{ability}</p>
-                            ))}
+        <div id="carousel" className="w-full py-[10rem] px-4 bg-white dark:bg-custom-black text-custom-dark dark:text-white relative">
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8 relative">
+                {characterIterator.current.slice(currentCardIndex, currentCardIndex + 3).map(
+                    (character, index) => (
+                        <div
+                            key={index}
+                            className={`w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 border border-oraange`}
+                        >
+                            <img className="w-220 mx-auto mt-[-3rem]" src={character.image} alt={character.name} />
+                            <h2
+                                className={`text-2xl font-bold text-center text-${character.color}-500 py-8`}
+                            >
+                                {character.name}
+                            </h2>
+                            <p className="text-center text-4xl font-bold">{character.type}</p>
+                            <div className="text-center font-medium">
+                                {character.abilities.map((ability, index) => (
+                                    <p key={index} className="py-2 pt-8 border-b mx-8">
+                                        {ability}
+                                    </p>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    )
+                )}
             </div>
             <div className="absolute top-1/2 -translate-y-1/2 left-8">
-                <button id="Left" className="text-3xl" onClick={goToPreviousCards}><FaArrowLeft /></button>
+                <button id="Left" className="text-3xl" onClick={goToPreviousCards}>
+                    <FaArrowLeft />
+                </button>
             </div>
             <div className="absolute top-1/2 -translate-y-1/2 right-8">
-                <button id="right" className="text-3xl" onClick={goToNextCards}><FaArrowRight /></button>
+                <button id="right" className="text-3xl" onClick={goToNextCards}>
+                    <FaArrowRight />
+                </button>
             </div>
         </div>
     );
